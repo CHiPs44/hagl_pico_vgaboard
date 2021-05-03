e39e3e4736cf0eb060940449380fd07394836da3
CHiPs44 <chips@chips44.fr>
2021-05-03 21:17:24 +0200

    Play with demo code

--- example/example.c
+++ example/example.c
@@ -47,36 +47,86 @@ SPDX-License-Identifier: MIT-0
 
 void example()
 {
-    int16_t counter = 0;
+    uint16_t counter = 0;
     wchar_t text[80];
+    uint16_t x0, y0, x1, y1;
+    uint16_t x = 64; //48;
+    uint16_t y = 64; //52;
+    int8_t dx = 1;
 
-    // for ( int x = 0; x < DISPLAY_WIDTH; ++x ) {
-    //     hagl_put_pixel(x,                  0, RGB565_WHITE);
-    //     hagl_put_pixel(x, DISPLAY_HEIGHT - 1, RGB565_WHITE);
+    // hagl_draw_hline(0, DISPLAY_HEIGHT / 2, DISPLAY_WIDTH, 15);
+    // hagl_draw_vline(DISPLAY_WIDTH / 2, 0, DISPLAY_HEIGHT - 1, 15);
+
+    // for ( int x = 8; x < DISPLAY_WIDTH - 8; ++x ) {
+    //     plot_point(x,          0 + 8, 15);
+    //     plot_point(x, HEIGHT - 1 - 8, 15);
     // }
-    // for ( int y = 0; y < DISPLAY_HEIGHT; ++y) {
-    //     hagl_put_pixel(                0, y, RGB565_WHITE);
-    //     hagl_put_pixel(DISPLAY_WIDTH - 1, y, RGB565_WHITE);
+    // for ( int y = 8; y < DISPLAY_HEIGHT - 8; ++y) {
+    //     plot_point(        0 + 8, y, 15);
+    //     plot_point(WIDTH - 1 - 8, y, 15);
     // }
-    hagl_draw_rectangle(0, 0, DISPLAY_WIDTH - 1, DISPLAY_HEIGHT - 1, palette[ 3]);
-    hagl_draw_rectangle(8, 8, DISPLAY_WIDTH - 8, DISPLAY_HEIGHT - 8, palette[11]);
-    for (uint8_t i = 0; i < NCLR; i++)
+
+    hagl_put_text(L"HAGL RASPBERRY PI PICO 640x480x16 DEMO", x, y, 15, font6x9);
+    hagl_draw_hline(x, y + 9, DISPLAY_WIDTH - 1 - 2 * x, 15);
+    y += 16;
+    uint8_t col;
+    uint8_t row;
+    for (uint8_t c = 0; c < 16; c++)
     {
-        swprintf(text, sizeof(text), L" #%02d => %04x", i, palette[i]);
-        hagl_put_text(text, 16, 16 + (i * 8), palette[15], font6x9);
+        // Nice coloured frames around screen
+        x0 = c * 4; x1 = DISPLAY_WIDTH  - 1 - c * 4;
+        y0 = c * 4; y1 = DISPLAY_HEIGHT - 1 - c * 4;
+        hagl_draw_rectangle(x0, y0, x1, y1, 15 - c);
+        // Framed tile + values for each color in the palette
+        col = c >= 8 ? 1 : 0;
+        row = c % 8;
+        x0 = x + col * (DISPLAY_WIDTH / 2) - col * (x - 4); x1 = x0 + 16;
+        y0 = y + row * 20                                 ; y1 = y0 + 16;
+        hagl_fill_rectangle(x0 + 1, y0 + 1, x1 - 1, y1 - 1, c);
+        hagl_draw_rectangle(x0, y0, x1, y1, 15);
+        /*
+        #define PICO_SCANVIDEO_PIXEL_RSHIFT 0
+        #define PICO_SCANVIDEO_PIXEL_GSHIFT 6
+        #define PICO_SCANVIDEO_PIXEL_BSHIFT 11
+        #define PICO_SCANVIDEO_PIXEL_FROM_RGB8(r, g, b) (
+            (((b)>>3)<<PICO_SCANVIDEO_PIXEL_BSHIFT)|
+            (((g)>>3)<<PICO_SCANVIDEO_PIXEL_GSHIFT)|
+            (((r)>>3)<<PICO_SCANVIDEO_PIXEL_RSHIFT))
+        */
+        uint8_t r = 0; //palette[c] & 0x
+        uint8_t g = 0; //palette[c] & 0x
+        uint8_t b = 0; //palette[c] & 0x
+        swprintf(text, sizeof(text), L"#%02d => %04x => %02x/%02x/%02x", c, palette[c], r, g, b);
+        hagl_put_text(text, x0 + 24, y0 + 3, 15, font6x9);
     }
 
+    y = DISPLAY_HEIGHT / 2 + 8;
+    x = 0;
     for(;;)
     {
         gpio_put(LED_PIN, counter % 2);
-        // "1234567890"
-        // " [123456] "
-        swprintf(text, sizeof(text), L" -<[%06d]>- ", counter);
-        hagl_put_text(text, 16 + (counter % (DISPLAY_WIDTH / 2)), 16 + (DISPLAY_HEIGHT / 2), RGB565_RED   , font6x9);
-        hagl_put_text(text, 32 + (counter % (DISPLAY_WIDTH / 2)), 32 + (DISPLAY_HEIGHT / 2), RGB565_GREEN , font6x9);
-        hagl_put_text(text, 48 + (counter % (DISPLAY_WIDTH / 2)), 48 + (DISPLAY_HEIGHT / 2), RGB565_BLUE  , font6x9);
-        hagl_put_text(text, 64 + (counter % (DISPLAY_WIDTH / 2)), 64 + (DISPLAY_HEIGHT / 2), RGB565_CYAN  , font6x9);
-        sleep_ms(10);
+        for (uint16_t n = 0; n < 16; n++)
+        {
+            uint16_t w = counter % (DISPLAY_WIDTH / 3) + n * 3;
+            hagl_draw_hline(DISPLAY_WIDTH / 2 - w, y + n * 4, w, n % 8 + counter % 8);
+            hagl_draw_hline(DISPLAY_WIDTH / 2    , y + n * 4, w, n % 8 + counter % 8);
+        }
+        for (uint16_t n = 0; n < 16; n++)
+        {
+            uint16_t w = counter % (DISPLAY_WIDTH / 3) - n * 3;
+            hagl_draw_hline(DISPLAY_WIDTH / 2 - w, 64 + y + n * 4, w, n % 8 + counter % 8);
+            hagl_draw_hline(DISPLAY_WIDTH / 2    , 64 + y + n * 4, w, n % 8 + counter % 8);
+        }
+        // swprintf(text, sizeof(text), L" -<[%06d]>- ", counter);
+        // for (uint8_t c = 1; c < NCLR; c++)
+        // {
+        //     hagl_put_text(text, 16 + 16 + x, 32 + 16 + (DISPLAY_HEIGHT / 2) + c * 10,  c, font6x9);
+        // }
+        // x += dx;
+        // if (x > DISPLAY_WIDTH - 16 - (10 * 9)) {
+        //     dx = -dx;
+        // }
+        sleep_ms(50);
         counter++;
     }
 }
