9a4fad44850d611fdd0615d64097e5bc0bbdfbb0
CHiPs44 <chips@chips44.fr>
2022-11-15 23:13:47 +0100

    Clock on demo + Grey palette

--- include/pico-vgaboard-palettes.h
+++ include/pico-vgaboard-palettes.h
@@ -44,15 +44,15 @@ extern "C"
 
 #define RAM __not_in_flash("pico_vgaboard_framebuffer")
 
-/* 16 colors default palette */
+/* 16 colors IRGB palette */
 /* Let's go for the 8 dark colors */
 #define IRGB_BLACK PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x00, 0x00, 0x00)
-#define IRGB_DARK_RED PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x80, 0x00, 0x00)
-#define IRGB_DARK_GREEN PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x00, 0x80, 0x00)
-#define IRGB_DARK_YELLOW PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x80, 0x80, 0x00)
-#define IRGB_DARK_BLUE PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x00, 0x00, 0x80)
-#define IRGB_DARK_MAGENTA PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x80, 0x00, 0x80)
-#define IRGB_DARK_CYAN PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x00, 0x80, 0x80)
+#define IRGB_DARK_RED PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x7f, 0x00, 0x00)
+#define IRGB_DARK_GREEN PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x00, 0x7f, 0x00)
+#define IRGB_DARK_YELLOW PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x7f, 0x7f, 0x00)
+#define IRGB_DARK_BLUE PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x00, 0x00, 0x7f)
+#define IRGB_DARK_MAGENTA PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x7f, 0x00, 0x7f)
+#define IRGB_DARK_CYAN PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x00, 0x7f, 0x7f)
 /* NB: light and dark grey are evenly distributed to make a 4 level grayscale with black and white */
 #define IRGB_DARK_GREY PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x55, 0x55, 0x55)
 /* And then the 8 brighter ones */
@@ -65,23 +65,27 @@ extern "C"
 #define IRGB_CYAN PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x00, 0xff, 0xff)
 #define IRGB_WHITE PICO_SCANVIDEO_PIXEL_FROM_RGB8(0xff, 0xff, 0xff)
 
+/** @brief White on Black */
 uint16_t RAM vgaboard_default_palette_1bpp[] = {
     /* 00 */ IRGB_BLACK,
     /* 01 */ IRGB_WHITE,
 };
 
+/** @brief Amstrad CPC Yellow on dark blue */
 uint16_t RAM vgaboard_amstrad_cpc_palette_1bpp[] = {
     /* 00 */ IRGB_DARK_BLUE,
     /* 01 */ IRGB_YELLOW,
 };
 
+/** @brief Amstrad CPC-ish Yellow and dark blue */
 uint16_t RAM vgaboard_default_palette_2bpp[] = {
     /* 00 */ IRGB_DARK_BLUE,
-    /* 01 */ IRGB_BLUE,
-    /* 02 */ IRGB_DARK_YELLOW,
+    /* 01 */ IRGB_DARK_YELLOW,
+    /* 02 */ IRGB_BLUE,
     /* 03 */ IRGB_YELLOW,
 };
 
+/** @brief White on black with two intermediate greys */
 uint16_t RAM vgaboard_grey_palette_2bpp[] = {
     /* 00 */ IRGB_BLACK,
     /* 01 */ IRGB_DARK_GREY,
@@ -89,6 +93,7 @@ uint16_t RAM vgaboard_grey_palette_2bpp[] = {
     /* 03 */ IRGB_WHITE,
 };
 
+/** @brief default, IRGB 16 colors palette with dark and light grey in order of luminosity */
 uint16_t RAM vgaboard_default_palette_4bpp[] = {
     /* 00 */ IRGB_BLACK,
     /* 01 */ IRGB_DARK_RED,
@@ -105,7 +110,27 @@ uint16_t RAM vgaboard_default_palette_4bpp[] = {
     /* 12 */ IRGB_BLUE,
     /* 13 */ IRGB_MAGENTA,
     /* 14 */ IRGB_CYAN,
-    /* 15 */ IRGB_WHITE};
+    /* 15 */ IRGB_WHITE
+};
+
+uint16_t RAM vgaboard_grey_palette_4bpp[] = {
+    /* 00 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x00, 0x00, 0x00),
+    /* 01 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x10, 0x10, 0x10),
+    /* 02 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x20, 0x20, 0x20),
+    /* 03 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x30, 0x30, 0x30),
+    /* 04 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x40, 0x40, 0x40),
+    /* 05 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x50, 0x50, 0x50),
+    /* 06 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x60, 0x60, 0x60),
+    /* 07 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x70, 0x70, 0x70),
+    /* 08 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x80, 0x80, 0x80),
+    /* 09 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x90, 0x90, 0x90),
+    /* 10 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0xa0, 0xa0, 0xa0),
+    /* 11 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0xb0, 0xb0, 0xb0),
+    /* 12 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0xc0, 0xc0, 0xc0),
+    /* 13 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0xd0, 0xd0, 0xd0),
+    /* 14 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0xe0, 0xe0, 0xe0),
+    /* 15 */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0xff, 0xff, 0xff)
+};
 
 // /* Specific to 8 bits depth / 256 colors mode */
 // uint16_t RAM vgaboard_default_palette_8bpp[256];
