f7f15ad8ad8c80354af535ffcacc54971e4d03a8
CHiPs <CHiPs44@users.noreply.github.com>
2023-01-27 11:26:05 +0100

    Update make_rgbi256.c

--- src/make_rgbi256.c
+++ src/make_rgbi256.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 
-/* cf. https://en.wikipedia.org/wiki/List_of_software_palettes#6-8-5_levels_RGB */
-
-/* Launch as gcc make-rgb685.c && ./a.out && rm a.out then copy/paste into pico-vgaboard-palettes.c */
+/* Launch as gcc make-rgbi256.c && ./a.out && rm a.out then copy/paste into pico-vgaboard-palettes.c */
 
 int main(void)
 {
-    int _r[] = { 0x00, 0x33, 0x66, 0x99, 0xcc, 0xff };
-    int _g[] = { 0x00, 0x24, 0x49, 0x6d, 0x92, 0xb6, 0xdb, 0xff };
-    int _b[] = { 0x00, 0x40, 0x80, 0xbf, 0xff };
-    // 240 rgb685 colors
-    for (int r = 0; r < 6; r++)
+    /*                          0          96         160         224        */
+    /*                          0           3           5           7        */
+    /*                        ---         ---         ---         ---        */
+    const uint8_t msb[4] = {0b00000000, 0b01100000, 0b10100000, 0b11100000};
+    /*                               0           8          16          24   */
+    /*                           -----       -----       -----       -----   */
+    const uint8_t lsb[4] = {0b00000000, 0b00001000, 0b00010000, 0b00011000};
+    uint8_t c = 0;
+    uint8_t r, g, b;
+    for (uint8_t _r = 0; _r < 4; _r++)
     {
-        for (int g = 0; g < 8; g++)
+        for (uint8_t _g = 0; _g < 4; _g++)
         {
-            for (int b = 0; b < 5; b++)
+            for (uint8_t _b = 0; _b < 4; _b++)
             {
-                int i = 40 * r + 5 * g + b;
-                printf(
-                    "    /* %03d */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x%02x, 0x%02x, 0x%02x),\n", 
-                    i, _r[r], _g[g], _b[b]
-                );
+                for (uint8_t _i = 0; _i < 4; _i++)
+                {
+                    r = msb[_r] | lsb[_i];
+                    g = msb[_g] | lsb[_i];
+                    b = msb[_b] | lsb[_i];
+                    printf(
+                        "    /* %03d */ PICO_SCANVIDEO_PIXEL_FROM_RGB8(0x%02x, 0x%02x, 0x%02x),\n", 
+                        c++, r, g, b
+                    );
+                }
             }
         }
     }
