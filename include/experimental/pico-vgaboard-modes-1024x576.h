a1b7885b00ed6dc2c82da0c123ccdc0468712a13
CHiPs44 <chips@chips44.fr>
2023-06-16 14:01:40 +0200

    TIC-80 mode?

---
+++ include/experimental/pico-vgaboard-modes-1024x576.h
@@ -0,0 +1,140 @@
+/*
+
+MIT License
+
+Copyright (c) 2021-2023 Christophe "CHiPs44" Petit
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-cut-
+
+This file is part of the Raspberry Pi Pico VGA board HAL for the HAGL graphics library:
+
+https://github.com/CHiPs44/hagl_pico_vgaboard
+
+https://github.com/tuupola/hagl
+
+SPDX-License-Identifier: MIT
+
+*/
+
+#ifndef _HAGL_PICO_VGABOARD_MODES_1024X576_H
+#define _HAGL_PICO_VGABOARD_MODES_1024X576_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "pico/scanvideo.h"
+#include "pico-vgaboard.h"
+
+#define VGABOARD_1024X576_PIXEL_CLOCK_HZ 65000000L
+#define VGABOARD_1024X576_SYS_CLOCK_KHZ  (4 * VGABOARD_1024X576_PIXEL_CLOCK_HZ / 1000L)
+#define VGABOARD_1024X576_FREQ_HZ 60
+
+/**
+ * @brief VGA timings for 1024x576@60Hz
+ */
+const scanvideo_timing_t vga_timing_1024x576_60_pico = {
+    .clock_freq = VGABOARD_1024X576_PIXEL_CLOCK_HZ,
+    .h_active = 1024,
+    .v_active = 768,
+    .h_front_porch = 24,
+    .h_pulse = 136,
+    .h_total = 1344,
+    .h_sync_polarity = 0,
+    .v_front_porch = 3,
+    .v_pulse = 6,
+    .v_total = 806,
+    // (@kilograham) .v_sync_polarity = 1,
+    .v_sync_polarity = 0,
+    .enable_clock = 0,
+    .clock_polarity = 0,
+    .enable_den = 0,
+};
+
+#define SCANVIDEO_MODE_1024x576(__xscale__, __yscale__) {\
+    .default_timing = &vga_timing_1024x576_60_pico,\
+    .pio_program = &video_24mhz_composable,\
+    .width = 1024,\
+    .height = 768,\
+    .xscale = (__xscale__),\
+    .yscale = (__yscale__),\
+}
+
+const scanvideo_mode_t vga_mode_1024x576_60_pico = SCANVIDEO_MODE_1024x576(1, 1);
+const scanvideo_mode_t vga_mode_1024x384_60_pico = SCANVIDEO_MODE_1024x576(1, 2);
+const scanvideo_mode_t vga_mode_512x768_60_pico  = SCANVIDEO_MODE_1024x576(2, 1);
+const scanvideo_mode_t vga_mode_512x384_60_pico  = SCANVIDEO_MODE_1024x576(2, 2);
+const scanvideo_mode_t vga_mode_512x192_60_pico  = SCANVIDEO_MODE_1024x576(2, 4);
+const scanvideo_mode_t vga_mode_256x384_60_pico  = SCANVIDEO_MODE_1024x576(4, 2);
+const scanvideo_mode_t vga_mode_256x192_60_pico  = SCANVIDEO_MODE_1024x576(4, 4);
+const scanvideo_mode_t vga_mode_128x96_60_pico   = SCANVIDEO_MODE_1024x576(8, 8);
+
+#define VGABOARD_1024x576(__scanvideo_mode__, __depth__, __palette__) {\
+    .scanvideo_mode = (__scanvideo_mode__),\
+    .freq_hz = VGABOARD_1024X576_FREQ_HZ,\
+    .depth = (__depth__),\
+    .palette = ((uint16_t *)(__palette__)),\
+    .sys_clock_khz = VGABOARD_1024X576_SYS_CLOCK_KHZ\
+}
+
+/***************************/
+/* 24576 BYTES FRAMEBUFFER */
+/***************************/
+
+/** @brief 512x384@60Hz, 1bpp, 2 colors, 24576 bytes framebuffer */
+const vgaboard_t vgaboard_512x384x1bpp_24576   = VGABOARD_1024x576(&vga_mode_512x384_60_pico, 1, &vgaboard_palette_1bpp_default);
+/** @brief 256x192@60Hz, 4bpp, 16 colors, 24576 bytes framebuffer */
+const vgaboard_t vgaboard_256x192x4bpp_24576_1 = VGABOARD_1024x576(&vga_mode_256x192_60_pico, 4, &vgaboard_palette_4bpp_default);
+/** @brief 128x96@60Hz, 16bpp, 32768 colors + 1 bit alpha - BGAR5515, 49152 bytes framebuffer */
+const vgaboard_t vgaboard_128x96x16bpp_24576   = VGABOARD_1024x576(&vga_mode_128x96_60_pico  , 16, &vgaboard_palette_16bpp_empty);
+
+/***************************/
+/* 49152 BYTES FRAMEBUFFER */
+/***************************/
+
+/** @brief 1024x384@60Hz, 1bpp, monochrome, 49152 bytes framebuffer */
+const vgaboard_t vgaboard_1024x384x1bpp  = VGABOARD_1024x576(&vga_mode_1024x576_60_pico,  1, &vgaboard_palette_1bpp_default);
+/** @brief 512x768@60Hz, 1bpp, monochrome, 49152 bytes framebuffer */
+const vgaboard_t vgaboard_512x768x1bpp   = VGABOARD_1024x576(&vga_mode_512x768_60_pico ,  1, &vgaboard_palette_1bpp_default);
+/** @brief 512x384@60Hz, 2bpp, 4 colors, 49152 bytes framebuffer */
+const vgaboard_t vgaboard_512x384x2bpp   = VGABOARD_1024x576(&vga_mode_512x384_60_pico ,  2, &vgaboard_palette_2bpp_default);
+/** @brief 512x192@60Hz, 4bpp, 16 colors, 49152 bytes framebuffer */
+const vgaboard_t vgaboard_512x192x4bpp   = VGABOARD_1024x576(&vga_mode_512x192_60_pico ,  4, &vgaboard_palette_4bpp_default);
+/** @brief 256x384@60Hz, 4bpp, 16 colors, 49152 bytes framebuffer */
+const vgaboard_t vgaboard_256x384x4bpp   = VGABOARD_1024x576(&vga_mode_256x384_60_pico ,  4, &vgaboard_palette_4bpp_default);
+/** @brief 256x192@60Hz, 8bpp, 256 colors, 49152 bytes framebuffer */
+const vgaboard_t vgaboard_256x192x8bpp_1 = VGABOARD_1024x576(&vga_mode_256x192_60_pico ,  8, &vgaboard_palette_8bpp_default);
+
+/***************************/
+/* 98304 BYTES FRAMEBUFFER */
+/***************************/
+
+/** @brief 1024x576@60Hz, 1bpp, monochrome, 98304 bytes framebuffer */
+const vgaboard_t vgaboard_1024x576x1bpp_98304 = VGABOARD_1024x576(&vga_mode_1024x576_60_pico, 1, &vgaboard_palette_1bpp_default);
+/** @brief 512x384@60Hz, 4bpp, 16 colors, 98304 bytes framebuffer */
+const vgaboard_t vgaboard_512x384x4bpp_98304  = VGABOARD_1024x576(&vga_mode_512x384_60_pico , 4, &vgaboard_palette_4bpp_default);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _HAGL_PICO_VGABOARD_MODES_1024X576_H */
