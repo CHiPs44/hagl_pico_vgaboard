e2def03e8b73a181ae16b40fdf24491c8acbd205
CHiPs44 <chips@chips44.fr>
2024-01-08 19:24:39 +0100

    Commander X16 256 colors default palette + various tests with 256 colors

---
+++ pico_vgaboard/include/pico-vgaboard-palettes-cx16.h
@@ -0,0 +1,56 @@
+/*
+
+MIT License
+
+Copyright (c) 2021-2024 Christophe "CHiPs44" Petit
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-cut-
+
+This file is part of the Raspberry Pi Pico VGA board HAL for the HAGL graphics library:
+
+https://github.com/CHiPs44/hagl_pico_vgaboard
+
+https://github.com/tuupola/hagl
+
+SPDX-License-Identifier: MIT
+
+*/
+
+#ifndef _HAGL_PICO_VGABOARD_PALETTES_CX16_H
+#define _HAGL_PICO_VGABOARD_PALETTES_CX16_H
+
+#include "pico-vgaboard.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* cf. https://github.com/X16Community/x16-emulator/blob/r46/src/video.c#L195 */
+
+/** @brief Commander X16 default palette */
+extern const BGAR5515 pico_vgaboard_palette_8bpp_cx16[256];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _HAGL_PICO_VGABOARD_PALETTES_CX16_H */
