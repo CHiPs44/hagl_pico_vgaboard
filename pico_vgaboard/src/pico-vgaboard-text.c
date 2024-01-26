3508626884d0019edf4f63f159434c02cf744333
CHiPs44 <chips@chips44.fr>
2024-01-26 13:55:18 +0100

    Text mode WIP 1

---
+++ pico_vgaboard/src/pico-vgaboard-text.c
@@ -0,0 +1,44 @@
+/*
+
+MIT License
+
+Copyright (c) 2021-2024 Christophe "CHiPs44" Petit
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-cut-
+
+This file is part of the Raspberry Pi Pico VGA board HAL for the HAGL graphics library:
+ - https://github.com/CHiPs44/hagl_pico_vgaboard
+ - https://github.com/tuupola/hagl
+
+SPDX-License-Identifier: MIT
+
+*/
+
+#include "pico-vgaboard-textmode.h"
+
+#include <stdio.h>
+
+pico_vgaboard_textmode_t pico_vgaboard_textmode;
+
+void pico_vgaboard_textmode_init()
+{
+    pico_vgaboard_textmode.
+}
