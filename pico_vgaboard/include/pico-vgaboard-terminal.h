fe3cf3434c873496a7ea034d6f279dc2b9975cfa
CHiPs44 <chips@chips44.fr>
2024-10-08 23:14:30 +0200

    Terminal WIP

--- pico_vgaboard/include/pico-vgaboard-terminal.h
+++ pico_vgaboard/include/pico-vgaboard-terminal.h
@@ -48,55 +48,144 @@ extern "C"
 {
 #endif
 
-   typedef enum e_pvtt_direction
-   {
-        DIR_RIGHT=0, DIR_UP=1, DIR_LEFT=2, DIR_DOWN=3
-    } pvtt_direction;
+    typedef enum e_pvtt_cursor_movement
+    {
+        MOVE_RIGHT,
+        MOVE_UP,
+        MOVE_LEFT,
+        MOVE_DOWN,
+        MOVE_END,
+        MOVE_TOP,
+        MOVE_HOME,
+        MOVE_BOTTOM
+    } pvtt_cursor_movement;
 
     typedef struct s_pvtt_terminal
     {
         pvts_screen *screen;
-        bool        scroll;
-    } pvts_terminal;
+        /** @brief scroll screen at top or bottom? */
+        bool scroll;
+        /** @brief just wrap at beginning and end of screen? */
+        bool wrap;
+    } pvtt_terminal;
 
-    static inline pvts_terminal *pvtt_init(pvts_screen *screen)
+    /** @brief Create a terminal object */
+    static inline pvts_screen *pvtt_terminal *pvtt_init(uint8_t cols, uint8_t rows)
     {
-    }
-
-    static inline void pvtt_done(pvts_terminal *term)
-    {
-    }
-
-    static inline void pvtt_move_cursor(pvts_terminal *term, int dir)
-    {
-     switch (dir) {
-         case DIR_RIGHT:
-           break;
-         case DIR_UP:
-        if (term->screen->row > 0)
+        pvts_screen *screen = pvts_init(cols, rows);
+        if (screen == NULL)
+            return NULL;
+        pvtt_terminal *term = calloc(1, sizeof(pvtt_terminal));
+        if (term == NULL)
         {
-             term->screen->row -= 1;
-        } else {
-             if (term->scroll)
-             {
-                 pvtt_scroll_up(term);
-             }
+            free(screen);
+            return NULL;
         }
-        break;
-         case DIR_LEFT:
-           break;
-         case DIR_DOWN:
-        if (term->screen->row < term->screen->rows - 1)
-        {
-             term->screen->row += 1;
-        } else {
-             if (term->scroll)
-             {
-                 pvtt_scroll_down(term);
-             }
-        }
-           break;
+        term->screen = screen;
+        scroll = false;
     }
+
+    /** @brief Free a terminal object */
+    static inline void pvtt_done(pvtt_terminal *term)
+    {
+        if (term->screen != NULL)
+        {
+            free(term->screen);
+        }
+        free(term);
+    }
+
+    static inline void pvtt_scroll_up(pvtt_terminal *term)
+    {
+
+    }
+
+    static inline void pvtt_move_cursor(pvtt_terminal *term, int move)
+    {
+        switch (move)
+        {
+        case MOVE_RIGHT:
+            // at end of line?
+            if (term->screen->col < term->screen->cols - 1)
+            {
+                // no, just move right
+                term->screen->col += 1;
+            }
+            else
+            {
+                if (term->wrap)
+                {
+                    term->screen->col = 0;
+                }
+                // at end of screen?
+                if (term->screen->row == term->screen->rows - 1 && term->scroll)
+                {
+                    term->screen->col = 0;
+                    pvtt_scroll_down(term);
+                }
+            }
+            break;
+        case MOVE_UP:
+            if (term->screen->row > 0)
+            {
+                // just go up
+                term->screen->row -= 1;
+            }
+            else
+            {
+                // at top of screen
+                if (term->wrap)
+                {
+                    // go to last line
+                    term->screen->row = term->screen->rows - 1;
+                }
+                else if (term->scroll)
+                {
+                    // insert blank line at top of screen
+                    pvtt_scroll_down(term);
+                }
+            }
+            break;
+        case MOVE_LEFT:
+            if (term->screen->col > 0)
+            {
+                term->screen->col -= 1;
+            }
+            else
+            {
+                // at start of line
+                if (term->wrap)
+                {
+                    // go to last column
+                    term->screen->col = term->screen->cols - 1;
+                }
+                // at start of screen?
+                if (term->scroll)
+                {
+                }
+            }
+            break;
+        case MOVE_DOWN:
+            if (term->screen->row < term->screen->rows - 1)
+            {
+                // just go down
+                term->screen->row += 1;
+            }
+            else
+            {
+                if (term->wrap)
+                {
+                    // go to first line
+                    term->screen->row = 0;
+                }
+                else if (term->scroll)
+                {
+                    // insert blank line at bottom of screen
+                    pvtt_scroll_up(term);
+                }
+            }
+            break;
+        }
     }
 
 #ifdef __cplusplus
